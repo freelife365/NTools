c7b1244a4e9b93cdff939f4f608a269f307aa37e
madsen-git <xu_ping_2003@163.com>
2018-09-29 08:48:31 +0800

    init

---
+++ device/devdef.h
@@ -0,0 +1,13 @@
+﻿#ifndef __DEV_DEVDEF_H__
+#define __DEV_DEVDEF_H__
+#pragma once
+#include "AC/sysupport.h"
+namespace dev {
+//////////////////////////////////////////////////////////////////////////
+//	2016-1-11	设备类型
+enum BEDeviceModel {
+	BEDeviceModelNone		= 0,
+	BEDeviceModelPort		,
+};
+}	// namespace dev
+#endif	// __DEV_DEVDEF_H__
