0461f70ce35a9214afb1815124a90abd55d869c9
madsen-git <xu_ping_2003@163.com>
2018-09-29 08:26:36 +0800

    clear

--- NTools/AC/MFC/MControl.cpp
+++
@@ -1,9 +0,0 @@
-#include "stdafx.h"
-#include "MControl.h"
-
-namespace acm {
-
-
-
-
-}
--- NTools/device/devinc.h
+++
@@ -1,9 +0,0 @@
-#ifndef __DEV_DEVINC_H__
-#define __DEV_DEVINC_H__
-#pragma once
-
-#include "devdef.h"
-#include "device.h"
-#include "port.h"
-
-#endif	// __DEV_DEVINC_H__
